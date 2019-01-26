7a5282cfee673c19e67d2f278b46dedbcae91025
jnbraun <jeannoel.braun@gmail.com>
2019-01-26 10:37:22 +0800

    Extended cifar10 example

--- examples/cifar10/cifar10_example.c
+++ examples/cifar10/cifar10_example.c
@@ -24,19 +24,10 @@
 
 #include "bcnn/bcnn.h"
 
-int create_network(bcnn_net *net) {
-    net->learner.optimizer = SGD;
-    net->learner.learning_rate = 0.005f;
-    net->learner.gamma = 0.00002f;
-    net->learner.decay = 0.0005f;
-    net->learner.momentum = 0.9f;
-    net->learner.policy = SIGMOID;
-    net->learner.step = 40000;
-    net->learner.beta1 = 0.9f;
-    net->learner.beta2 = 0.999f;
-    net->max_batches = 50000;
+typedef enum { SIMPLENET, RESNET18 } model_type;
 
-    bcnn_net_set_input_shape(net, 28, 28, 3, 128);
+static int simple_net(bcnn_net *net) {
+    bcnn_net_set_input_shape(net, 32, 32, 3, 128);
 
     bcnn_add_convolutional_layer(net, 32, 3, 1, 1, 1, 1, XAVIER, RELU, 0,
                                  "input", "conv1_1");
@@ -64,11 +55,96 @@ int create_network(bcnn_net *net) {
                         "label", "cost");
 
     bcnn_compile_net(net);
+    return 0;
+}
+
+static int resnet18(bcnn_net *net) {
+    bcnn_net_set_input_shape(net, 32, 32, 3, 32);
+
+    bcnn_add_convolutional_layer(net, 64, 3, 1, 1, 1, 1, XAVIER, RELU, 0,
+                                 "input", "conv1");
+    // Block 1_1
+    bcnn_add_convolutional_layer(net, 64, 3, 1, 1, 1, 1, XAVIER, RELU, 0,
+                                 "conv1", "conv1_1");
+    bcnn_add_convolutional_layer(net, 64, 3, 1, 1, 1, 1, XAVIER, NONE, 0,
+                                 "conv1_1", "conv1_2");
+    bcnn_add_eltwise_layer(net, RELU, "conv1", "conv1_2", "conv1_add1");
+    // Block 1_2
+    bcnn_add_convolutional_layer(net, 64, 3, 1, 1, 1, 1, XAVIER, RELU, 0,
+                                 "conv1_add1", "conv1_3");
+    bcnn_add_convolutional_layer(net, 64, 3, 1, 1, 1, 1, XAVIER, NONE, 0,
+                                 "conv1_3", "conv1_4");
+    bcnn_add_eltwise_layer(net, RELU, "conv1_add1", "conv1_4", "conv1_add2");
+    // Block 2_1
+    bcnn_add_convolutional_layer(net, 128, 3, 2, 1, 1, 1, XAVIER, RELU, 0,
+                                 "conv1_add2", "conv2_1");
+    bcnn_add_convolutional_layer(net, 128, 3, 1, 1, 1, 1, XAVIER, NONE, 0,
+                                 "conv2_1", "conv2_2");
+    bcnn_add_convolutional_layer(net, 128, 1, 2, 0, 1, 1, XAVIER, NONE, 0,
+                                 "conv1_add2", "conv2_res1");
+    bcnn_add_eltwise_layer(net, RELU, "conv2_res1", "conv2_2", "conv2_add1");
+    // Block 2_2
+    bcnn_add_convolutional_layer(net, 128, 3, 1, 1, 1, 1, XAVIER, RELU, 0,
+                                 "conv2_add1", "conv2_3");
+    bcnn_add_convolutional_layer(net, 128, 3, 1, 1, 1, 1, XAVIER, NONE, 0,
+                                 "conv2_3", "conv2_4");
+    bcnn_add_eltwise_layer(net, RELU, "conv2_add1", "conv2_4", "conv2_add2");
+    // Block 3_1
+    bcnn_add_convolutional_layer(net, 256, 3, 2, 1, 1, 1, XAVIER, RELU, 0,
+                                 "conv2_add2", "conv3_1");
+    bcnn_add_convolutional_layer(net, 256, 3, 1, 1, 1, 1, XAVIER, NONE, 0,
+                                 "conv3_1", "conv3_2");
+    bcnn_add_convolutional_layer(net, 256, 1, 2, 0, 1, 1, XAVIER, NONE, 0,
+                                 "conv2_add2", "conv3_res1");
+    bcnn_add_eltwise_layer(net, RELU, "conv3_res1", "conv3_2", "conv3_add1");
+    // Block 3_2
+    bcnn_add_convolutional_layer(net, 256, 3, 1, 1, 1, 1, XAVIER, RELU, 0,
+                                 "conv3_add1", "conv3_3");
+    bcnn_add_convolutional_layer(net, 256, 3, 1, 1, 1, 1, XAVIER, NONE, 0,
+                                 "conv3_3", "conv3_4");
+    bcnn_add_eltwise_layer(net, RELU, "conv3_add1", "conv3_4", "conv3_add2");
+    // Block 4_1
+    bcnn_add_convolutional_layer(net, 512, 3, 2, 1, 1, 1, XAVIER, RELU, 0,
+                                 "conv3_add2", "conv4_1");
+    bcnn_add_convolutional_layer(net, 512, 3, 1, 1, 1, 1, XAVIER, NONE, 0,
+                                 "conv4_1", "conv4_2");
+    bcnn_add_convolutional_layer(net, 512, 1, 2, 0, 1, 1, XAVIER, NONE, 0,
+                                 "conv3_add2", "conv4_res1");
+    bcnn_add_eltwise_layer(net, RELU, "conv4_res1", "conv4_2", "conv4_add1");
+    // Block 4_2
+    bcnn_add_convolutional_layer(net, 512, 3, 1, 1, 1, 1, XAVIER, RELU, 0,
+                                 "conv4_add1", "conv4_3");
+    bcnn_add_convolutional_layer(net, 512, 3, 1, 1, 1, 1, XAVIER, NONE, 0,
+                                 "conv4_3", "conv4_4");
+    bcnn_add_eltwise_layer(net, RELU, "conv4_add1", "conv4_4", "conv4_add2");
+    //////////////////
+    bcnn_add_avgpool_layer(net, "conv4_add2", "pool");
+    // bcnn_add_maxpool_layer(net, 4, 4, PADDING_SAME, "conv4_add2", "pool");
+    bcnn_add_fullc_layer(net, 10, XAVIER, NONE, 0, "pool", "fc");
+    bcnn_add_softmax_layer(net, "fc", "softmax");
+    bcnn_add_cost_layer(net, EUCLIDEAN_LOSS, COST_ERROR, 1.0f, "softmax",
+                        "label", "cost");
+
+    bcnn_compile_net(net);
+
+    return 0;
+}
+
+int create_network(bcnn_net *net, model_type type) {
+    net->learner.optimizer = ADAM;
+    net->learner.learning_rate = 0.005f;
+    net->learner.gamma = 0.00002f;
+    net->learner.decay = 0.0005f;
+    net->learner.momentum = 0.9f;
+    net->learner.policy = SIGMOID;
+    net->learner.step = 40000;
+    net->learner.beta1 = 0.9f;
+    net->learner.beta2 = 0.999f;
 
     // Data augmentation
-    net->data_aug.range_shift_x = 10;
-    net->data_aug.range_shift_y = 10;
-    net->data_aug.rotation_range = 20.0f;
+    net->data_aug.range_shift_x = 6;
+    net->data_aug.range_shift_y = 6;
+    net->data_aug.rotation_range = 15.0f;
     net->data_aug.max_brightness = 60;
     net->data_aug.min_brightness = -60;
     net->data_aug.max_contrast = 1.5f;
@@ -77,6 +153,19 @@ int create_network(bcnn_net *net) {
 
     // Target
     net->prediction_type = CLASSIFICATION;
+
+    // Define the network topology
+    switch (type) {
+        case SIMPLENET:
+            simple_net(net);
+            break;
+        case RESNET18:
+            resnet18(net);
+            break;
+        default:
+            simple_net(net);
+            break;
+    }
 
     return 0;
 }
@@ -115,18 +204,6 @@ int predict_cifar10(bcnn_net *net, char *test_img, float *error, int nb_pred,
             fprintf(f, "\n");
         }
     }
-    // Last predictions (Have to do this because batch_size is set to 16 yet the
-    // number of samples of mnist test data is not a multiple of 16)
-    n = nb_pred % net->batch_size;
-    if (n > 0) {
-        for (i = 0; i < n; ++i) {
-            bcnn_predict_on_batch(net, &data_iter, &out, &error_batch);
-            err += error_batch;
-            // Save predictions
-            for (k = 0; k < output_size; ++k) fprintf(f, "%f ", out[k]);
-            fprintf(f, "\n");
-        }
-    }
     *error = err / nb_pred;
 
     if (f != NULL) fclose(f);
@@ -155,7 +232,7 @@ int train_cifar10(bcnn_net *net, char *train_img, char *test_img, int nb_iter,
         if (i % eval_period == 0 && i > 0) {
             bh_timer_stop(&t);
             bh_timer_start(&tp);
-            predict_cifar10(net, test_img, &error_valid, 128,
+            predict_cifar10(net, test_img, &error_valid, 10000,
                             "predictions_cifar10.txt");
             bh_timer_stop(&tp);
             fprintf(stderr,
@@ -178,17 +255,17 @@ int train_cifar10(bcnn_net *net, char *train_img, char *test_img, int nb_iter,
     return 0;
 }
 
-int run(char *train_data, char *test_data) {
+int run(char *train_data, char *test_data, model_type model) {
     float error_train = 0.0f, error_test = 0.0f;
     bcnn_net *net = NULL;
 
     bcnn_init_net(&net);
     net->state = TRAIN;
     BCNN_INFO(net->log_ctx, "Create Network...");
-    create_network(net);
+    create_network(net, model);
 
     BCNN_INFO(net->log_ctx, "Start training...");
-    if (train_cifar10(net, train_data, test_data, 4000000, 5, &error_train) !=
+    if (train_cifar10(net, train_data, test_data, 4000000, 500, &error_train) !=
         0) {
         BCNN_ERROR(net->log_ctx, -1, "Can not perform training");
     }
@@ -206,9 +283,21 @@ int run(char *train_data, char *test_data) {
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <train_data> <test_data>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <train_data> <test_data> [model]\n",
+                argv[0]);
+        fprintf(stderr, "\t Required:\n");
+        fprintf(stderr,
+                "\t\t <train_data> : path to cifar10 training "
+                "binary file\n");
+        fprintf(stderr,
+                "\t\t <test_data> : path to cifar10 testing "
+                "binary file\n");
+        fprintf(stderr, "\t Optional:\n");
+        fprintf(stderr, "\t\t [model]: network model type: (default: 0)\n");
+        fprintf(stderr, "\t\t\t0: Simple CNN\n");
+        fprintf(stderr, "\t\t\t1: Resnet18\n");
         return -1;
     }
-    run(argv[1], argv[2]);
+    run(argv[1], argv[2], (model_type)(argc > 3 ? atoi(argv[3]) : 0));
     return 0;
 }

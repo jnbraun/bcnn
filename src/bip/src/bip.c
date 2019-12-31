3825c4fa8639dd4dd5083f0a34880e3df7fd8d6f
jean <jeannoel.braun@gmail.com>
2019-12-31 09:22:22 +0800

    Prevent multiple definitions of stb_image

--- src/bip/src/bip.c
+++ src/bip/src/bip.c
@@ -26,8 +26,10 @@
 #include <limits.h>
 
 #ifdef BIP_USE_STB_IMAGE
+#define STB_IMAGE_STATIC
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image/stb_image.h>
+#define STB_IMAGE_WRITE_STATIC
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image/stb_image_write.h>
 #endif
